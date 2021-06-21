65444831f4382c94bd4acd27519753929af5f376
BenaminOchi <latidos3@gmail.com>
2021-06-20 20:46:32 -0500

    Create Big and Small Bot projects

---
+++ VexAiSmallBot01/src/MysteryGang/IsolationMode.cpp
@@ -0,0 +1,194 @@
+#include "MysteryGang/CommonPartMethods.h"
+#include "MysteryGang/IsolationMode.h"
+#include "MysteryGang/RobotConfig.h"
+
+extern void setJetsonDisplay(int lr);
+extern void getBoxData(bool* hasTarget, int* classId, int* x, int* y, float* widthI, float* heightI, float* depthI);
+
+static const char* sBuildDate = __DATE__;
+static const char* sBuildTime = __TIME__;
+static const char* sVersion = "0001";
+
+static bool sHasTarget       = false;
+static int   sCurClassId     = 0;
+static int   sCurX           = 0;
+static int   sCurY           = 0;
+static float sCurWidthI      = 0.0;
+static float sCurHeightI     = 0.0;
+static float sCurDepthI      = 0.0;
+
+static int sLeftMotorSpeed   = 0;
+static int sRightMotorSpeed  = 0;
+
+static bool sIsTaskSuspended = false;
+static bool sIsTracking = true;
+
+namespace IsolationMode {
+
+  void enterMain() {
+    thread tIsol(IsolationMode::controlTask);
+  }
+
+  const char* getBuildDate() {
+    return sBuildDate;
+  }
+
+  const char* getBuildTime() {
+	return sBuildTime;
+  }
+
+  const char* getVersion() {
+	return sVersion;
+  }
+
+  void setWheelSpeeds() {
+    const unsigned int centerX = 320;
+    const int MaxSpeed = 7;  //TBD: 150;
+    const int HalfSpeed = 4;  //TBD: 75;
+    int targetDistance = sCurDepthI;
+    int leftRight = 3;  // Default to: pointing at target
+    static bool sIsTargetInRange = false;
+
+    if (sHasTarget == false) {
+      leftRight = 0;    // no valid target
+      if (sIsTargetInRange == false) {
+        Cpm::coastWheels();
+      }
+      else {
+        LimitSwitchD.pressed(Cpm::setLimitSwitchPressed);
+        BumperC.pressed(Cpm::setBumperSwitchPressed);
+        if (Cpm::wasLimitSwitchPressed() == true) {
+          Cpm::stopAllMotors();
+          vex::task::sleep(3000);
+          //Cpm::clearLimitSwitchPressed();
+          //vex::task::sleep(3000);
+          //Cpm::startTopIntakesReverse();
+          //vex::task::sleep(500);
+          //Cpm::stopTopIntakes();
+          sIsTracking = false;
+        }
+        if (Cpm::wasBumperSwitchPressed() == true) {
+          Cpm::stopAllMotors();
+          sIsTracking = false;
+        }
+      }
+    }
+    //else if (sCurX <= 130) {
+    else if (sCurX <= (centerX - 100 * 2)) {
+      sLeftMotorSpeed = MaxSpeed * -1;
+      sRightMotorSpeed = MaxSpeed;
+      leftRight = 1;    // Left
+    }
+    //else if (sCurX >= 190) {
+    else if (sCurX >= (centerX + 100 * 2)) {
+      sLeftMotorSpeed = MaxSpeed;
+      sRightMotorSpeed = MaxSpeed * -1;
+      leftRight = 2;    // Right
+    }
+    //else if (sCurX <= 155) {
+    else if (sCurX <= (centerX - 60 * 2)) {
+      sLeftMotorSpeed = HalfSpeed *  -1;
+      sLeftMotorSpeed = HalfSpeed;
+      leftRight = 1;    // Left
+    }
+    //else if (sCurX >= 165) {
+    else if (sCurX >= (centerX + 60 * 2)) {
+      sLeftMotorSpeed = HalfSpeed;
+      sRightMotorSpeed = HalfSpeed * -1;
+      leftRight = 2;    // Right
+    }
+    else {  // We are pretty much pointing at the target
+      // Here the robot is pointed at the target, move forward to target
+      if (targetDistance >= 30) {
+        sLeftMotorSpeed = 40;
+        sRightMotorSpeed = 40;
+      }
+      else if (targetDistance >= 16) {
+        sLeftMotorSpeed = 30;
+        sRightMotorSpeed = 30;
+      }
+      //else if (targetDistance >= 1) {
+      else {
+        sLeftMotorSpeed = 15;
+        sRightMotorSpeed = 15;
+        Cpm::startBottomIntakes();
+        Cpm::startMiddleIntake();
+        sIsTargetInRange = true;
+      }
+    }
+    setJetsonDisplay(leftRight);
+  }
+
+  //-----------------------
+  // Isolation mode event loop
+  //-----------------------
+  int controlTask() {
+
+    Cpm::stopAllMotors();
+
+    Cpm::moveRobotForward(24);
+    vex::task::sleep(200);
+    Cpm::turnRobotLeft(135);
+    //Cpm::turnRobotRight(135);
+
+    // Enter the main control loop
+    while (true) {
+      if (sIsTracking == true) {
+        // Get latest info on the target
+        getBoxData(&sHasTarget, &sCurClassId, &sCurX, &sCurY, &sCurWidthI, &sCurHeightI, &sCurDepthI);
+
+        setWheelSpeeds();
+
+        if (sIsTracking == true) {
+          // Move wheels based on trackloop positions
+          hwMotorWheelFrontLeft.spin(vex::directionType::fwd, sLeftMotorSpeed, vex::velocityUnits::pct);
+          hwMotorWheelFrontRight.spin(vex::directionType::fwd, sRightMotorSpeed, vex::velocityUnits::pct);
+          hwMotorWheelBackLeft.spin(vex::directionType::fwd, sLeftMotorSpeed, vex::velocityUnits::pct);
+          hwMotorWheelBackRight.spin(vex::directionType::fwd, sRightMotorSpeed, vex::velocityUnits::pct);
+        }
+      }
+
+      if (sIsTaskSuspended == true) {
+        break;
+      }
+      vex::task::sleep(1);  // Allow detection of competition switch, etc.
+    }
+    return 0;
+  }
+
+  void suspendTask() {
+    sIsTaskSuspended = true;
+  }
+
+  void testCpm() {
+    Cpm::stopAllMotors();
+
+    Cpm::moveRobotForward(24);
+    vex::task::sleep(200);
+    Cpm::turnRobotLeft(135);
+    vex::task::sleep(200);
+    Cpm::moveRobotForward(500);
+    vex::task::sleep(200);
+    Cpm::moveRobotBackward(500);
+    vex::task::sleep(200);
+    Cpm::turnRobotLeft(2000);
+    vex::task::sleep(200);
+    Cpm::turnRobotRight(2000);
+    vex::task::sleep(200);
+    Cpm::startAllIntakes();
+    vex::task::sleep(500);
+    Cpm::stopAllIntakes();
+    vex::task::sleep(1000);
+    Cpm::startBottomIntakes();
+    vex::task::sleep(500);
+    Cpm::stopBottomIntakes();
+    vex::task::sleep(1000);
+    Cpm::startMiddleIntake();
+    vex::task::sleep(500);
+    Cpm::stopMiddleIntake();
+    vex::task::sleep(1000);
+    Cpm::startTopIntakes();
+    vex::task::sleep(500);
+    Cpm::stopTopIntakes();
+  }
+}
---
+++ VexAiSmallBot01/src/main.cpp
@@ -0,0 +1,148 @@
+#include "vex.h"
+#include "MysteryGang/CommonPartMethods.h"
+#include "MysteryGang/CurConfig.h"
+#include "MysteryGang/InteractiveMode.h"
+#include "MysteryGang/IsolationMode.h"
+#include "MysteryGang/ManualMode.h" 
+#include "MysteryGang/RobotConfig.h"
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+
+
+// create instance of jetson class to receive location and other
+// data from the Jetson nano
+//
+ai::jetson  jetson_comms;
+
+static bool sFirstAutoFlag = true;
+
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                          Auto_Isolation Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous isolation  */
+/*  phase of a VEX AI Competition.                                           */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void auto_Isolation(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+  IsolationMode::enterMain();
+}
+
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                        Auto_Interaction Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous interaction*/
+/*  phase of a VEX AI Competition.                                           */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+
+void auto_Interaction(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+  InteractiveMode::enterMain();
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                          AutonomousMain Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+void autonomousMain(void) {
+  // ..........................................................................
+  // The first time we enter this function we will launch our Isolation routine
+  // When the field goes disabled after the isolation period this task will die
+  // When the field goes enabled for the second time this task will start again
+  // and we will enter the interaction period. 
+  // ..........................................................................
+
+  if (sFirstAutoFlag) {
+    hwBrain.Screen.print("Enter Isolation Mode");
+    hwBrain.Screen.newLine();
+    auto_Isolation();
+  }
+  else { 
+    hwBrain.Screen.print("Enter Interactive Mode");
+    hwBrain.Screen.newLine();
+    auto_Interaction();
+  }
+  sFirstAutoFlag = false;
+}
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+  Cpm::stopAllMotors();
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+void enterUserControl(void) {
+  ManualMode::enterManualControl();
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  int32_t loop_time = 66;     // Run at about 15Hz
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  CurConfig::initialize();
+  CurConfig::displayConfigBrain();
+
+  thread tDash(dashboardTask);   // Start the status update display
+  tDash.setPriority(thread::threadPrioritylow);
+
+  // Set up callbacks for autonomous and driver control periods.
+  //hwCompetition.autonomous(autonomousMain);
+  IsolationMode::enterMain();  // TEMP for debug
+  //enterUserControl();          // TEMP for debug
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    // request new data    
+    // NOTE: This request should only happen in a single task.    
+    jetson_comms.request_map();
+
+    // Allow other tasks to run
+    this_thread::sleep_for(loop_time);
+  }
+}
